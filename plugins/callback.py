3a28200e7c99ade3cae4b94eae17703a677d7fb2
waly <85253290+jackblose@users.noreply.github.com>
2021-09-27 21:21:53 +0700

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -97,8 +97,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/wysupportinfo'),
+                InlineKeyboardButton('💝 Donate', url='https://t.me/enjoyyourmilk'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
