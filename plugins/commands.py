91f8aeb7d7d52ac269128e2e421a779bb58c4c72
waly <85253290+jackblose@users.noreply.github.com>
2021-09-27 21:26:32 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -29,8 +29,8 @@ admin_filter=filters.create(is_admin)
 async def start(client, message):
     buttons = [
         [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot')
+            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/wysupportinfo'),
+            InlineKeyboardButton('💝 Donate', url='https://t.me/enjoyyourmilk')
         ],
         [
             InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help'),
@@ -45,8 +45,8 @@ async def start(client, message):
 async def show_help(client, message):
     buttons = [
         [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot'),
+            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/wysupportinfo'),
+            InlineKeyboardButton('💝 Donate', url='https://t.me/enjoyyourmilk'),
         ]
         ]
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -60,8 +60,8 @@ async def show_help(client, message):
 async def repo_(client, message):
     buttons = [
         [
-            InlineKeyboardButton('🧩 Repository', url='https://github.com/subinps/VCPlayerBot'),
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
+            InlineKeyboardButton('✨ Join Now', url='https://t.me/temanesiaonline'),
+            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/wysupportinfo'),
             
         ],
     ]
